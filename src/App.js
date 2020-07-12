4043986b3b9085e55e999165094743b3338457f5
Jem Pimentel <jem.pimentel@outlook.com>
2020-07-13 00:41:29 +0800

    Adds prettier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-const Pet = ({name, animal, breed}) => {
+const Pet = ({ name, animal, breed }) => {
   return React.createElement('div', {}, [
     React.createElement('h1', {}, name),
     React.createElement('h2', {}, animal),
     React.createElement('h2', {}, breed),
-  ]);
-};
+  ])
+}
 
 const App = () => {
   return React.createElement('div', { id: 'an-id' }, [
@@ -24,7 +24,7 @@ const App = () => {
       animal: 'bird',
       breed: 'Awesome',
     }),
-  ]);
-};
+  ])
+}
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById('root'))
