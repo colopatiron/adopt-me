d51088edb5253cc8279dab5920e2eb50c88a5ef6
Jem Pimentel <jem.pimentel@outlook.com>
2020-07-14 00:53:02 +0800

    Adds breed dropdown

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -4,6 +4,8 @@ import { ANIMALS } from '@frontendmasters/pet'
 const SearchParams = () => {
   const [location, setLocation] = useState('New York, NY')
   const [animal, setAnimal] = useState('dog')
+  const [breed, setBreed] = useState('')
+  const [breeds, setBreeds] = useState([])
 
   return (
     <div className='search-params'>
@@ -31,6 +33,23 @@ const SearchParams = () => {
             {ANIMALS.map((animal) => (
               <option key={animal} value={animal}>
                 {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor='breed'>
+          Breed
+          <select
+            id='breed'
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+            disabled={!breeds.length}
+          >
+            <option>All</option>
+            {breeds.map((breed) => (
+              <option key={breed} value='{breed}'>
+                {breed}
               </option>
             ))}
           </select>
